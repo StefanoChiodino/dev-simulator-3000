b6dcf656447cd4337d4d905fd16f9a888b01c939
Stefano Chiodino <stefano.chiodino@infinityworks.com>
2020-11-20 19:59:16 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use cursive::views::TextView;
+
+struct Job {
+    work_units_cost: i32,
+    title: String,
+}
+
+struct Dev {
+    name: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let job = Job { work_units_cost: 100, title: "Fix typo".to_string() };
+
+	let mut siv = cursive::default();
+
+	siv.add_global_callback('q', |s| s.quit());
+    let mut text = TextView::new(job.title);
+	siv.add_layer(text);
+
+    siv.run();
+    text.
 }
